ecd2ecd6c87337b9e9820a16dd4434db4e29bb8d
Sinrock <mruhaak@gmail.com>
2020-08-17 00:10:57 -0600

    iteration alterations

--- lib/Strainz.rb
+++ lib/Strainz.rb
@@ -5,21 +5,15 @@ class StrainSearchCLI
       puts "Welcome to Strainz!".green
       puts "You can search strain names, races, effects and flavors by keyword!".light_blue
       puts "Which term(s) would you like to search for? (Type exit to quit the program)".light_blue
-      #input = gets.strip
-      #puts "Gathering the data you requested...".yellow
       strain_create
-      #matches
   end
-
       def strain_create
-        input = gets.strip
+        input = gets.chomp.downcase
         puts "Gathering the data you requested...".yellow
         query = StrainImporter.new("https://strainapi.evanbusse.com/rvxnT8j/strains/search/all").parse_json
+        #I'd like this part to iterate over the hash of query and only return the results whose values match the choice of input by the user
         query.each do |k,v|
-          if v == "#{input}"
-          puts "#{k}"end end
-        #new_strains = Strain.new("#{query}").save
-        #puts JSON.parse(new_strains)#.parse_json #.select { |k,v| v == "#{input}"}
+          puts "#{k} "end
       end   
 end
 end
--- lib/strain.rb
+++ lib/strain.rb
@@ -1,42 +1,25 @@
 module Strainz
     
 class Strain
-    attr_accessor :id, :name, :race, :effect, :flavor
+    attr_accessor :id, :name, :race, :effects, :flavors
 
     @@all = []
 
-    def initialize(arg)#id="none", name="none", race="none", effects="none", flavors="none")
-        #@arg = arg
-        #@id = id
-        #@name = name
-        #@race = race
-        #@effects = effects
-        #@flavors = flavors
-        #self.id = id
-        #self.name = name
-        #self.race = race
-        #self.effect = effect
-        #self.flavor = flavor
-        #final = new_strains.select { |key,value| value == "#{input}"}
-        #puts final
-        #puts id
-        #puts name
-        #puts race
-        #puts effects
-        #puts flavors
+    def initialize(arg)
+        
+        @id = id
+        @name = name
+        @race = race
+        @effects = effects
+        @flavors = flavors
+        @@all << self
     end
-    #def parse_json
-    #    JSON.parse(@alternative)
-    #    #puts self.parse_json
-    #end
-    
     def self.all
-    @@all
-  end
-  def save
-    self.class.all << self
-  end
-
-    #new_strains.select { |key,value| value == "#{input}"}
+        @@all
+    end
+    def save
+        self.class.all << self
+    end
 end
 end
+ #new_strains.select { |key,value| value == "#{input}"}
