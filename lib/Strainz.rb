070ef45577fd4c39044bb8d415cdf98fd2fa44d4
Sinrock <mruhaak@gmail.com>
2020-08-21 13:48:16 -0600

    working finally, fixed hash in strain.rb

--- lib/Strainz.rb
+++ lib/Strainz.rb
@@ -1,73 +1,19 @@
 module Strainz
 
 class CLI
-  include DeepFind
-
   def run
     system("clear")
       puts "Welcome to Strainz!".green
-      puts "You can search strain names, races, effects and flavors by keyword!".light_blue
-      puts "Which term(s) would you like to search for?".light_blue
+      puts "Search for the attributes of any marijuana strain!".light_blue
+      puts "What's the name of the strain you'd like info on?".light_blue
       strain_create
-      
+      # (type exit to quit)
   end
       def strain_create
-        input = gets.chomp.downcase
+        input = gets.chomp.downcase.split.map(&:capitalize).join(' ')
         puts "Gathering the data you requested...".yellow
         query = StrainImporter.new("https://strainapi.evanbusse.com/rvxnT8j/strains/search/all").parse_json
-        #pass_to_strain = flatten_hash(query).flatten
-        #puts pass_to_strain #Strain.new(pass_to_strain)
-        #puts nested_hash_value(query,"#{input}")
-       #puts deep_find(query, "#{input}", nested_key: "value")
-       #puts recursive(query, "#{input}")
-       #puts query.has_value?("#{input}")
        puts testing = Strain.new(query, "#{input}").strainprint
       end
-
-  def recursive(hash, key)
-    return hash[key] if key != Hash
-    if key.is_a? Hash
-      key.each do |(k, v), h|
-        if v.is_a? Hash 
-          h.each do |(h_k, h_v), h_h|
-          end
-          else
-            hash[key] = v
-          end
-        end
-      end
-  end
-  
-  def search_hash(hash, key)
-    return hash[key] if hash.assoc(key)
-    hash.delete_if{|key, value| value.class != Hash}
-    new_hash = Hash.new
-    hash.each_value {|values| new_hash.merge!(values)}
-    unless new_hash.empty?
-    search_hash(new_hash, key)
-  end
-end
-#def nested_hash_value(obj,key)
-#  if obj.respond_to?(:key?) && obj.key?(key)
-#    obj[key]
-#  elsif obj.respond_to?(:each)
-#    r = nil
-#    obj.find{ |*a| r=nested_hash_value(a.last,key) }
-#    r
-#  end
-#end
-
-
-def flatten_hash(h)
-  h.each_with_object({}) do |(k, v), h|
-    if v.is_a? Hash
-      flatten_hash(v).map do |h_k, h_v|
-        h["#{k}-#{h_k}".to_s] = h_v
-      end
-    else 
-      h[k] = v
-    end
-   end
-end
 end
 end
--- lib/strain.rb
+++ lib/strain.rb
@@ -1,36 +1,26 @@
 class Strain
+  include Strainz
     attr_accessor :id, :race, :effects, :flavors, :name
     @@all = []
 
    def initialize(hash, key)
-    hash.each do |name, data|
-      data.each.to_s do |attribute, value| 
-        value.each do |attribute_two, value_two|
-        end
-      end
-    end
-    end
-    @id = id
-    @race = race
-    @effects = effects
-    @flavors = flavors
-    @name = name
+    fetch = hash.fetch(key)
+    @id = fetch.fetch("id")
+    @race = fetch.fetch("race")
+    @effects = fetch.fetch("effects")
+    @flavors = fetch.fetch("flavors")
+    @name = key
     @@all << self
    end
 
    def strainprint
-    puts race
+    puts "Strain Name:".magenta + "#{name}".green
+    puts "Strain Id:".magenta + "#{id}".green
+    puts "Strain Race:".magenta + "#{race}".green
+    puts "Strain Effects:".magenta + ":#{effects}".green
+    puts "Strain Flavors:".magenta + "#{flavors}".green
    end
    def self.all
     @@all
    end
   end
-
-    #def initialize(*h)
-   # if h.class == (Array)
-   #     h.first.each { |k,v| public_send("#{k}=",v) }
-   # end
-  #end
-
-  #def findmatchingstrains(array)
-  #  located_strain = array.detect {||}
