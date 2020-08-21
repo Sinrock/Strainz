e0bb44173f9858e7bd1c3d52c0daf1521fa86f62
Sinrock <mruhaak@gmail.com>
2020-08-21 15:02:50 -0600

    fix errors

--- lib/Strainz.rb
+++ lib/Strainz.rb
@@ -1,19 +1,30 @@
 module Strainz
-
-class CLI
-  def run
-    system("clear")
-      puts "Welcome to Strainz!".green
-      puts "Search for the attributes of any marijuana strain!".light_blue
-      puts "What's the name of the strain you'd like info on?".light_blue
-      strain_create
-      # (type exit to quit)
-  end
-      def strain_create
-        input = gets.chomp.downcase.split.map(&:capitalize).join(' ')
-        puts "Gathering the data you requested...".yellow
-        query = StrainImporter.new("https://strainapi.evanbusse.com/rvxnT8j/strains/search/all").parse_json
-       puts testing = Strain.new(query, "#{input}").strainprint
+  class CLI
+    def run
+      system("clear")
+      @user_input = nil
+      welcome
+      until @user_input == "exit"
+        main_menu
       end
-end
-end
+    end
+    def welcome
+      puts "Welcome to Strainz!".green
+    end
+      def main_menu
+        puts "Search for the attributes of any marijuana strain!".light_blue
+        puts "What's the name of the strain you'd like info on?(type exit to quit the application".light_blue
+        strain_create
+      end
+        def strain_create
+          @user_input = gets.chomp.downcase.split.map(&:capitalize).join(' ')
+          if @user_input.downcase == "exit"
+            exit
+          else
+            puts "Gathering the data you requested...".yellow
+            query = StrainImporter.new("https://strainapi.evanbusse.com/rvxnT8j/strains/search/all").parse_json
+          puts testing = Strain.new(query, "#{@user_input}").strainprint
+          end
+        end
+    end
+  end
--- lib/strain.rb
+++ lib/strain.rb
@@ -4,20 +4,24 @@ class Strain
     @@all = []
 
    def initialize(hash, key)
+    if hash.has_key?(key)
     fetch = hash.fetch(key)
+    #if key not found return as much
     @id = fetch.fetch("id")
     @race = fetch.fetch("race")
     @effects = fetch.fetch("effects")
     @flavors = fetch.fetch("flavors")
     @name = key
     @@all << self
+    else puts "No Strain by that name exists. As far as I know of.".red
    end
+  end
 
    def strainprint
     puts "Strain Name:".magenta + "#{name}".green
     puts "Strain Id:".magenta + "#{id}".green
     puts "Strain Race:".magenta + "#{race}".green
-    puts "Strain Effects:".magenta + ":#{effects}".green
+    puts "Strain Effects:".magenta + "#{effects}".green
     puts "Strain Flavors:".magenta + "#{flavors}".green
    end
    def self.all
