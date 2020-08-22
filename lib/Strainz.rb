bc7d9635ea069b4b9a2d92432e57800cf023c31a
Sinrock <mruhaak@gmail.com>
2020-08-21 19:15:39 -0600

    completely workinggit add .!

--- lib/Strainz.rb
+++ lib/Strainz.rb
@@ -1,8 +1,8 @@
 module Strainz
   class CLI
     def run
-      data_hash = StrainImporter.new("https://strainapi.evanbusse.com/rvxnT8j/strains/search/all").parse_json
-      Strain.create_all_strains(data_hash)
+      @data_hash = StrainImporter.new("https://strainapi.evanbusse.com/rvxnT8j/strains/search/all").parse_json
+      Strain.create_all_strains(@data_hash)
       system("clear")
       @user_input = nil
       welcome
@@ -24,8 +24,20 @@ module Strainz
             exit
           else
             puts "Gathering the data you requested...".yellow
-            #query = StrainImporter.new("https://strainapi.evanbusse.com/rvxnT8j/strains/search/all").parse_json
-          puts testing = Strain.new(query, "#{@user_input}").strainprint
+            users_choice = Strain.find_by_name(@user_input)
+            if users_choice != nil
+              then
+            puts "Strain Name: ".magenta + users_choice.name.green
+            puts "Strain Id: ".magenta + users_choice.id.to_s.green
+            puts "Strain Race: ".magenta + users_choice.race.green
+            puts "Strain Effects: ".magenta + users_choice.effects.to_s.green
+            puts "Strain Flavors: ".magenta + users_choice.flavors.to_s.green
+            else 
+              puts "No strains with that name found in the database!".red
+            end
+
+            #need to find the selected strain based on user input find by name method return 1 strain
+            #write if statement if found, print, if not return error message strain not found
           end
         end
     end
--- lib/strain.rb
+++ lib/strain.rb
@@ -1,34 +1,28 @@
 class Strain
-  #include Strainz
     attr_accessor(:id, :race, :effects, :flavors, :name)
     @@all = []
 
    def initialize(key, hash)
-    
-    #if hash.has_key?(key)
-    #fetch = hash.fetch(key)
-    #if key not found return as much
     @id = hash["id"]
     @race = hash["race"]
     @effects = hash["effects"]
     @flavors = hash["flavors"]
     @name = key
     @@all << self
-    #else puts "No Strain by that name exists. As far as I know of.".red
-   #end
   end
+
   def self.create_all_strains(api_call)
   #iterate over the hash we get back
   #for each of the strains in the hash instantiate a new object
   api_call.each do |key, data|
     self.new(key, data)
-
-  end
-  binding.pry
+  end 
 end
 
-def self.find_by_name(strains)
-@@all.find {|args|}
+def self.find_by_name(query_name)
+@@all.find {|strain| strain.name == query_name}
+  
+end
 end
 
    def strainprint
@@ -41,4 +35,3 @@ end
    def self.all
     @@all
    end
-  end
